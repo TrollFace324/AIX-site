3c5e8d8b533515001825f74b82c44a8b7238b717
Matvey Timofeev <mntimofeev07@gmail.com>
2026-03-02 02:55:25 +0500

    Фикс бага отображения 3D

---
+++ three-addons.d.ts
@@ -0,0 +1,11 @@
+declare module 'three/addons/loaders/DRACOLoader.js' {
+    export * from 'three/examples/jsm/loaders/DRACOLoader.js';
+}
+
+declare module 'three/addons/loaders/GLTFLoader.js' {
+    export * from 'three/examples/jsm/loaders/GLTFLoader.js';
+}
+
+declare module 'three/addons/controls/OrbitControls.js' {
+    export * from 'three/examples/jsm/controls/OrbitControls.js';
+}
